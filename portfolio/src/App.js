4b5a8389731d11284eab163aa52c50410ed57638
Keita Suzuki <ksuzuki417@gmail.com>
2020-12-11 21:31:19 -0600

    initial files created

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
